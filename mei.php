1e1615e6b2ce9902f0005e3d21744c89ed3e6af7
meicookies <79348524+meicookies@users.noreply.github.com>
2022-01-22 13:37:03 +0700

    Add files via upload

---
+++ mei.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title> Hacked by ./meicookies</title>
+  <style>
+    body {
+      background-color: white;
+    }
+    .img {
+      text-align: center;
+     }
+    .h1 {
+      color: #123456;
+      text-align: center;
+      font-family: monospace;
+	  font-size: 20px;
+    }
+    .img img {
+      height: 300px;
+      width: 300px;
+    }
+    .p1 {
+      text-align: center;
+      color: green;
+      font-family: monospace;
+    }
+  </style>
+</head>
+<body>
+  <div class="img">
+    <img src="https://l.top4top.io/p_21475jf9s0.jpg" alt="whut?">
+  </div>
+  <div class="h1">
+		Pwned by ./meicookies
+  </div>
+  <div class="p1">
+    <p>	
+	  <?php echo php_uname(); ?> <br /> <br />
+      Reason? just for fun... <br /><br />
+      AnonSec Team | Indonesian Hacktivist <br />
+      <marquee width="50%"> Anon7 | L4N4N9_4K1R4 | exa_gans77 | VenoRyan | MR.HAGAN_4040CR4ZY | PohonSagu | ./s3nt1n3L | DemonArmy | FarisGanss | KosameAmegai | CompuByte | B3bek_Gans | Mr.Froggy | #mrlinkerrorsystem | K4TSUY4_GH05T | Malaikat Hati | He4l3rz | Mr.Grim | PsychoH4x0r | AlgiSec1337 | Jamet1337 | Rian Haxor</marquee>
+    </p>
+	<audio id="audio" src="https://files.anon7.xyz/uploads/MjAyMjAxMjEwODM2/Bring%20Me%20The%20Horizon%20-%20Can%20You%20Feel%20My%20Heart.mp3" autoplay="false"></audio>
+	<button onclick="playsound();">CLICK HERE</button>
+  </div>
+<script>
+  function playsound() {
+    var audio = document.getElementById("audio");
+	audio.play();
+  }
+</script>
+</body>
+</html>
